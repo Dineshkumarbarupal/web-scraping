19d073cbab220ecf6cd989779c657e605beea20a
vihaan barupal <159883311+Dineshkumarbarupal@users.noreply.github.com>
2024-08-04 11:48:16 +0530

    Update data_collect.py

--- data_collect.py
+++ data_collect.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
 import os
 
+d = {'tital': [1, 2], 'price': [3, 4],  "link": [1 , 2]}
+
 for file in os.listdir("data"):
     with open (f"data/{file}") as f:
      html_doc = f.read()
 
     soup = BeautifulSoup(html_doc, 'html.parser')
-    print(soup.prettify())
-
-
+    t = soup.find("h2")
+    tital = t.get_text()
+    print(tital)
+    break
+    
 
